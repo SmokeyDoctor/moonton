137d7fdf6c366ecebe6cbccf68482834c798c31b
Smoking Docktor <sidiqjafar45@gmail.com>
2023-06-30 15:59:52 +0700

    feat: instalasi spatie middleware

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,13 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('admin', function (){
+//     return 'Admin';
+// } )->middleware('role:admin');
+
+// Route::get('user', function (){
+//     return 'User';
+// } )->middleware('role:user');
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
